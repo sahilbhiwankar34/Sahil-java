b968b51eb4195784ea7e345d5708c590c9fcc0dc
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-01 20:48:02 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,9 @@
+class simple{
+   public static void main(String[] args) {
+     int t=9;
+     int pi=89;
+     int r=90;
+     int si=t+pi+r/100;
+     System.out.println("Simple interest: "+si);  
+   }
+} 
