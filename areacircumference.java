97bc4c9b71611896180de6a844f0fa7be4cf0933
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-01 20:47:29 +0530

    Add files via upload

---
+++ areacircumference.java
@@ -0,0 +1,11 @@
+class areacircumference{
+    public static void main(String args[]){
+        double PI=3.142;
+        float radius=50;
+        double area=PI*radius*radius;
+        double circum=2*PI*radius;
+        System.out.println("Area= "+area);
+        System.out.println("Circumference= "+circum);
+
+    }
+}
