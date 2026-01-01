f78d293d8e9f462d84eb3f9c8d54eb2253523cca
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-01 20:48:21 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,15 @@
+class swapping {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Before swapping a= "+a);
+        System.out.println("Before swapping b= "+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("********************");
+        System.out.println("After swapping a= "+a);
+        System.out.println("After swapping b= "+b);
+
+    }
+}
