0280fb84e81b8204de8deffa046f7ad5f2c52adb
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-01 20:47:47 +0530

    Add files via upload

---
+++ marksheet1.java
@@ -0,0 +1,14 @@
+ class marksheet1 {
+    public static void main(String args[]){
+int m1=90;
+int m2=87;
+int m3=87;
+int m4=76;
+int m5=34;
+int sum=m1+m2+m3+m4+m5;
+float per=sum/5;
+System.out.println("Total: "+sum);
+System.out.println("Percentage: "+per);
+
+    }
+}
