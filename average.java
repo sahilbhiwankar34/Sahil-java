eef7c165ec505b2e43dc60d8e82a7496a886dbc6
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2026-01-03 20:17:35 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,9 @@
+public class average {
+    public static void main(String[] args) {
+        int n1=90;
+        int n2=98;
+        int n3=100;
+        double avg=n1+n2+n3/3;
+        System.out.println("Average= "+avg);
+    }
+}
