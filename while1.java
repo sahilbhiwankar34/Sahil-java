57acf39ceea8dec2f17b28fa38c8945754b2999a
Sahil Bhiwankar <sahilbhiwankar34@gmail.com>
2026-01-27 22:06:02 +0530

    Add files via upload

---
+++ while1.java
@@ -0,0 +1,22 @@
+class while1{
+public static void main(String[] args){
+    int n=20;
+    int i=1;
+    System.out.print("Even numbers are: ");
+    while(i<=n){
+        if(i%2==0){
+           System.out.println(i); 
+        }
+    i++;
+    }
+    i=1;
+    System.out.println("odd numbers are: ");
+    while(i<=n){
+        if(i%2!=0){
+        System.out.println(i);  
+        }
+    i++;
+    }
+}
+}
+    
