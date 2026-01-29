9e2324d9e27193bb21da5d294f82deaadcbc5d90
Sahil <sahilbhiwankar34@gmail.com>
2026-01-29 18:47:30 +0530

    Person

---
+++ Person.java
@@ -0,0 +1,103 @@
+class Person1{
+    int age;
+    String name;
+    char gender;
+    float Salary;
+    String post;
+    int experience;
+
+}
+public class Person {
+  public static void main(String[] args) {
+      Person1 p1=new Person1();
+      Person1 p2=new Person1();
+      Person1 p3=new Person1();
+      Person1 p4=new Person1();
+      Person1 p5=new Person1();
+      //person1
+      p1.age=22;
+      p1.name="Mohit";
+      p1.gender='M';
+      p1.Salary=4500000;
+      p1.post="Manager";
+      p1.experience=3;
+      //person2
+      p2.age=23;
+      p2.name="Sahil";
+      p2.gender='M';
+      p2.Salary=1500000;
+      p2.post="Accounts";
+      p2.experience=3;
+      //person3
+      p3.age=24;
+      p3.name="Aditya";
+      p3.gender='M';
+      p3.Salary=4500000;
+      p3.post="Manager";
+      p3.experience=2;
+      //person4
+      p4.age=25;
+      p4.name="Om";
+      p4.gender='M';
+      p4.Salary=4500000;
+      p4.post="Builder";
+      p4.experience=4;
+      //person5
+      p5.age=26;
+      p5.name="Ayush";
+      p5.gender='M';
+      p5.Salary=4500000;
+      p5.post="Manager";
+      p5.experience=4;
+
+      System.out.println("Persons Information");
+      //Printing the details of persons
+      //Display person 1 details
+        System.out.println("Name: "+p1.name);
+        System.out.println("Age: "+p1.age);
+        System.out.println("Gender: "+p1.gender);
+        System.out.println("Salary: "+p1.Salary);
+        System.out.println("Post: "+p1.post);
+        System.out.println("Work Experience: "+p1.experience);
+        System.out.println();
+        //Display person 2 details
+        System.out.println("Name: "+p2.name);
+        System.out.println("Age: "+p2.age);
+        System.out.println("Gender: "+p2.gender);
+        System.out.println("Salary: "+p2.Salary);
+        System.out.println("Post: "+p2.post);
+        System.out.println("Work Experience: "+p2.experience);
+        System.out.println();
+        //Display person 3 details
+        System.out.println("Name: "+p3.name);
+        System.out.println("Age: "+p3.age);
+        System.out.println("Gender: "+p3.gender);
+        System.out.println("Salary: "+p3.Salary);
+        System.out.println("Post: "+p3.post);
+        System.out.println("Work Experience: "+p3.experience);
+        System.out.println();
+        //Display person 4 details
+        System.out.println("Name: "+p4.name);
+        System.out.println("Age: "+p4.age);
+        System.out.println("Gender: "+p4.gender);
+        System.out.println("Salary: "+p4.Salary);
+        System.out.println("Post: "+p4.post);
+        System.out.println("Work Experience: "+p4.experience);
+        System.out.println();
+        //Display person 5 details
+        System.out.println("Name: "+p5.name);
+        System.out.println("Age: "+p5.age);
+        System.out.println("Gender: "+p5.gender);
+        System.out.println("Salary: "+p5.Salary);
+        System.out.println("Post: "+p5.post);
+        System.out.println("Work Experience: "+p5.experience);
+        System.out.println();
+
+
+      
+
+
+
+
+  }  
+}
