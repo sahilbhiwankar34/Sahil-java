5b59b3761daf9a1a691e7ffc7e902ea59c325b8e
Sahil Bhiwankar <sahilbhiwankar34@gmail.com>
2026-06-16 21:15:55 +0530

    Add files via upload

---
+++ Reverse_number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Reverse_number{
+    public static void main(String args[]){
+        try(Scanner sc=new Scanner(System.in)){
+            System.out.println("Enter the number: ");
+            int num=sc.nextInt();
+            int rev=0;
+            while(num!=0){
+                int rem=num%10;
+                rev=rev*10+rem;
+                num=num/10;
+            }
+            System.out.println("Reverse of the number is: "+rev);
+
+        }
+    }
+}
